545bc63aec5310887f3787638d8a5be951b48e45
fcesc-code <bpfran@gmail.com>
2021-10-31 09:12:10 +0100

    feature / add components for todo module and TodoDTO type

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TodoListComponent, TodoListItemComponent, TodoAddComponent],
+  imports: [CommonModule],
 })
-export class TodoModule { }
+export class TodoModule {}
